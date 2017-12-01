bcf82ec64c1d01cd733a9f16fb409a20c9bd6455
wenyumei <wenyumei@zhumengyuan>
2017-12-01 15:31:45 +0800

    element-ui插件container容器实例

--- src/components/element.vue
+++ src/components/element.vue
@@ -36,7 +36,7 @@
         </el-submenu>
 
         <el-submenu index="3">
-          <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+          <template slot="title"><i class="el-icon-share"></i>导航三</template>
           <el-menu-item-group>
             <template slot="title">分组一</template>
             <el-menu-item index="3-1">选项1</el-menu-item>
